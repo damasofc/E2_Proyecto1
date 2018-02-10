fa154e5a18a767ea2bddb4994bb988cfe5e2e92c
damasofc <damasofc@hotmail.com>
2018-02-10 11:46:23 -0600

    menu

--- main.cpp
+++ main.cpp
@@ -2,8 +2,30 @@
 
  
 using namespace std;
+
+int menuPrincipal(){
+    int opc;
+    cout << "\nPROYECTO FILE SYSTEM\n"<<endl;
+    cout <<"\t1)Crear Disco\n\t2)Utilizar Disco Existente\n\t3)Salir"<<endl;
+    cout<<"\tEscriba su opcion: ";
+    cin>>opc;
+    switch (opc)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+    }
+    return opc;
+}
 int main()
 {
+    int i;
+    do
+    {
+        i = menuPrincipal();
+
+    } while(i!=3);
 
             
     return 0;
