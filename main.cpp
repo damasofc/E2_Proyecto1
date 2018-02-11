41d42d6fbf4d8da55a776edcec7a71a5e2c35c20
damasofc <damasofc@hotmail.com>
2018-02-11 11:44:01 -0600

    Agregando mas cosas

--- main.cpp
+++ main.cpp
@@ -3,17 +3,38 @@
  
 using namespace std;
 
+int menuDiscoExistente(string discName){
+    cout<<"\n\tPROYECTO FILE SYSTEM"<<endl;
+    cout<<"\tDisco << "<<discName<<" >>"<<endl;
+
+    return 1;
+}
 int menuPrincipal(){
-    int opc;
+    int opc,opcDiscoExistente;
     cout << "\nPROYECTO FILE SYSTEM\n"<<endl;
     cout <<"\t1)Crear Disco\n\t2)Utilizar Disco Existente\n\t3)Salir"<<endl;
     cout<<"\tEscriba su opcion: ";
     cin>>opc;
+    int cantEntradas,cantBloques;
+    string nombreDisco;
     switch (opc)
     {
         case 1:
+            cout<<"\nPROYECTO FILE SYSTEM"<<endl;
+            cout<<"\t\tCrear Disco"<<endl;
+            cout<<"\t\tIngrese Cantidad de Entradas: ";
+            cin>>cantEntradas;
+            cout<<"\n\t\tIngrese Cantidad de Bloques: ";
+            cin>>cantBloques;
             break;
         case 2:
+            do
+            {
+                cout<<"\t\tIngrese Nombre del Disco: ";
+                cin>>nombreDisco;
+                opcDiscoExistente = menuDiscoExistente(nombreDisco);
+            } while (opcDiscoExistente != 7);
+            
             break;
     }
     return opc;
