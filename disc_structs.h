bf0ad04057f0890079558fcfd986ab77ce8e37bb
damasofc <damasofc@hotmail.com>
2018-02-20 09:03:31 -0600

    crearDisco Listo

--- disc_structs.h
+++ disc_structs.h
@@ -1,6 +1,7 @@
 #ifndef E2_PROYECTO1_DISC_STRUCTS_H
 #define E2_PROYECTO1_DISC_STRUCTS_H
 
+
 struct data_block
 {
     char data[1020];
@@ -11,7 +12,7 @@ struct file_entry
 {
     char nombre[30];
     int tamano;
-    char tipo; // 'D' Directorio  o  'A' Archivo
+    char tipo[1]; // 'D' Directorio  o  'A' Archivo
     int padre;
     int primer_hijo;
     int hermano_derecho;
--- functions.h
+++ functions.h
@@ -26,57 +26,54 @@ int turnBitOn(unsigned char byte, int position)
 	return (byte |= (1 << position));	
 }
 
-void createDisc(string name, int cantEntradas, int cantBloques)
+bool createDisc(string name, int cantEntradas, int cantBloques)
 {
-	int posPuntero = 0;
     string nombreDisco = name + ".dat";
 	if(existFile(nombreDisco.c_str()))
 	{
-		return;
+		return false;
 	}
 	int bmSize = cantBloques / 8;
-	int tamanoBloque = 1024;
 	ofstream out(nombreDisco.c_str(),ios::in | ios::out | ios::binary);
 	out.open(nombreDisco.c_str());
 	//metaData
-	out.write((char*)&bmSize,4);
-	out.write((char*)&cantEntradas,4);
-	out.write((char*)&tamanoBloque,4);
-	out.write((char*)&cantBloques,4);
+	METADATA meta;
+	meta.bm_size = bmSize;
+	meta.entry_amount = cantEntradas;
+	meta.block_size = 1024;
+	meta.block_amount = cantBloques;
+	out.write(reinterpret_cast<char *> (&meta),sizeof(meta));
 	//Bitmap
-	out.write((char*)&cantBloques,4);
 	char* bitmap = new char[bmSize];
 	for(int i = 0; i < bmSize; i++)
 	{
 		bitmap[i] = 0;
 	}
 	out.write(bitmap,bmSize);
-	posPuntero = 20 + bmSize +1;
-	//escribir informacion vacia de files entries
-	file_entry fat[cantEntradas];
 	for(int i = 0; i < cantEntradas; i++)
 	{
+		file_entry fat;
 		for(int m = 0; m < 30; m++)
 		{
-			fat[i].nombre[m] = '-';
+			fat.nombre[m] = '-';
 		}
-		fat[i].tamano = 0;
-		fat[i].tipo = 'O';
-		fat[i].padre = -1;
-		fat[i].primer_hijo = -1;
-		fat[i].hermano_derecho = -1;
-		fat[i].primer_bloque_data = -1;
-		fat[i].libre = true;
-		//out.write(reinterpret_cast<const char*>(&fat[i]),sizeof(file_entry));
+		fat.tamano = 0;
+		fat.tipo[0] = 'O';
+		fat.padre = -1;
+		fat.primer_hijo = -1;
+		fat.hermano_derecho = -1;
+		fat.primer_bloque_data = -1;
+		fat.libre = true;
+		out.write(reinterpret_cast<const char*>(&fat),sizeof(fat));
 	}
-	out.write(reinterpret_cast<const char*>(fat),sizeof(fat));
 	//escribir informacion vacia de bloques de data
+	cout<<"sizeof de data block: "<<sizeof(data_block)<<endl;
 	for(int i = 0; i < cantBloques; i++)
 	{
-		data_block* bloque = new data_block();
-		bloque->siguiente = -1;
-		out.write((char*)bloque,sizeof(bloque));
+		data_block bloque;
+		bloque.siguiente = -1;
+		out.write(reinterpret_cast<char *>(&bloque),sizeof(bloque));
 	}
-	return;
+	return true;
 }
 
